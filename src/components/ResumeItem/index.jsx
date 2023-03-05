e9f307863bfacba49fb72f6fde3cf8199e0609ad
luciano soares <lucianosoaresdev748@gmail.com>
2023-03-05 18:10:51 -0300

    Finish resume Item

--- src/components/ResumeItem/index.jsx
+++ src/components/ResumeItem/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import * as C from "./styles";
 
-const ResumeItem = () => {
-  return <div>ResumeItem</div>;
+const ResumeItem = ({ title, Icon, value }) => {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderTitle>{title}</C.HeaderTitle>
+        <Icon />
+        <C.Total>{value}</C.Total>
+      </C.Header>
+    </C.Container>
+  );
 };
 
 export default ResumeItem;
