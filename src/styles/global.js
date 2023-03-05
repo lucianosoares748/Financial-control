c750ca4d267b7ca03679e339de167759922f8e0d
luciano soares <lucianosoaresdev748@gmail.com>
2023-03-05 17:30:50 -0300

    Header Styled Component ERROR

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,10 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const Global = createGlobalStyle`
-
-  body {
+  * {
     margin: 0;
     padding: 0;
+  }
+  
+  body {
     font-family: 'Poppins', Sans-serif;
     background-color: #f2f2f2;
   }
